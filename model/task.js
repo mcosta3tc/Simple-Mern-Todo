5f75a659e6fcbf458654dd6fbd6674ae99d6e654
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-12-02 11:29:55 +0100

    procfile added

---
+++ model/task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: String
+})
+
+const taskModel = mongoose.model('Task', taskSchema);
+
+module.exports = taskModel;
--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const taskRouter = express.Router()
+const taskRouter = express.Router();
+const taskModel = require('../model/task');
 
 taskRouter.post("/api/task/", async (req, res)=> {
-    console.log(req.body);
-    res.sendStatus(200).end();
+    const {title} = req.body;
+    let createdTask = new taskModel({title});
+    await createdTask.save();
 })
 
 module.exports = taskRouter;
