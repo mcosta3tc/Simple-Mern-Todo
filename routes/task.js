8369e185df6d757f66f6dc109613f43870e61259
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-12-02 11:46:42 +0100

    eslint prettier added

--- routes/task.js
+++ routes/task.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 const taskRouter = express.Router();
-const taskModel = require('../model/task');
+const taskModel = require("../model/task");
 
-taskRouter.post("/api/task/", async (req, res)=> {
-    const {title} = req.body;
-    let createdTask = new taskModel({title});
-    await createdTask.save();
-})
+taskRouter.post("/api/task/", async (req, res) => {
+    const { title } = req.body;
+    const createdTask = new taskModel(title);
+    try {
+        await createdTask.save();
+        console.log("Task Added");
+        res.sendStatus(200).send(createdTask);
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 module.exports = taskRouter;
