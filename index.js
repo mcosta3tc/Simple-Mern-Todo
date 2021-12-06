694d0f7f3e1a11bf89a017e73c4858ea5a775465
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-12-06 14:22:32 +0100

    cors added

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const taskRouter = require('./routes/task');
 const cors = require('cors');
 
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+    })
+);
 app.use(express.json());
-app.use(cors());
 app.use(taskRouter);
 
 const start = async () => {
     try {
-        await mongoose.connect(process.env.db_uri);
+        await mongoose.connect(process.env.URI);
         console.log('DB Connected');
         app.listen(port, () => {
             console.log(`Listen on : ${port}`);
